568bef146f3f2291d85e8652c34df61c7bc606d3
hiroki-0001 <s19c1091dk@s.chibakoudai.jp>
2022-01-03 20:24:43 +0900

    add check_kick.wbt & controller

--- controllers/check_kickmotion/check_kickmotion.py
+++ controllers/check_kickmotion/check_kickmotion.py
@@ -32,21 +32,19 @@ import traceback
 import transforms3d
 import random
 import numpy as np
-import csv
-
+import csv  # csvファイルの操作を楽にしてくれる
 from scipy.spatial import ConvexHull
-
 from types import SimpleNamespace
+from skopt import gp_minimize
 
 # start the webots supervisor
 supervisor = Supervisor()
 time_step = int(supervisor.getBasicTimeStep())
-
 field = Field("kid")
 children = supervisor.getRoot().getField('children')
 children.importMFNodeFromString(-1, f'RobocupSoccerField {{ size "kid" }}')
 children.importMFNodeFromString(-1, f'DEF BALL RobocupSoccerBall {{ translation 0 0 0.1 size 1 }}')
-children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.3 0 0.450 rotation 0 0 1 0 controller "play_motion" controllerArgs "./kick_motion0.csv"}}')
+children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.3 0 0.450 rotation 0 0 1 0 controller "play_motion" controllerArgs "motion.csv"}}')
 player = supervisor.getFromDef('PLAYER')
 ball = supervisor.getFromDef('BALL')
 player_translation = supervisor.getFromDef('PLAYER').getField('translation')
@@ -55,25 +53,36 @@ player_controller = supervisor.getFromDef('PLAYER').getField('controller')
 ball_translation = supervisor.getFromDef('BALL').getField('translation')
 ball_rotation = supervisor.getFromDef('BALL').getField('rotation')
 
-try:
-    for x in np.arange(-0.35, -0.1, 0.01):
-        for y in np.arange(0, 0.25, 0.01):
-            count = 0
-            player.remove()
-            children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation {x} {y} 0.450 rotation 0 0 1 0 controller "play_motion" controllerArgs "./kick_motion0.csv"}}')
-            player = supervisor.getFromDef('PLAYER')
-            ball.resetPhysics()
-            ball_translation.setSFVec3f([0, 0, 0.1])
-            ball_rotation.setSFRotation([0, 0, 1, 0])
-            while supervisor.step(time_step) != -1:
-                count += 1
-                if count > 800:
-                    break
-                if count > 800 - 1:
-                    pos = ball_translation.getSFVec3f()
-                    print(str(x)+", "+str(y)+", "+str(pos[0])+", "+str(pos[1]))
-                    with open('result.csv', 'a', newline='') as f:
-                        writer = csv.writer(f)
-                        writer.writerow([x, y, pos[0], pos[1]])
-except Exception:
-    error(f"Unexpected exception in main referee loop: {traceback.format_exc()}", fatal=True)
+def file_update(ball_pos):
+    with open("./ball_posion.csv", "a") as f:  # motion.csvに上書きする(前の状態から更新される)
+        f.write(str(ball_pos))  # 1行ずつ書き込み
+        f.write('\n')
+
+    
+def main():  # 最適化関数の設定
+    global player, ball, children, ball_translation, ball_rotation, supervisor  # グローバル変数の定義
+    player.remove()
+    children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.2 0.1 0.450 rotation 0 0 1 0 controller "play_motion" controllerArgs "motion.csv"}}')
+    player = supervisor.getFromDef('PLAYER')
+    ball.resetPhysics()
+    ball_translation.setSFVec3f([0, 0, 0.1])
+    ball_rotation.setSFRotation([0, 0, 1, 0])
+    count = 0
+
+    while supervisor.step(time_step) != -1:
+        count += 1
+        if count > 800:
+            break
+        if count > 800 - 1:
+            ball_pos = ball_translation.getSFVec3f()
+            print(ball_pos[0])
+            file_update(ball_pos[0])
+
+trials_num = 0
+while trials_num <= 10:
+    trials_num += 1
+    main()
+    print(trials_num)
+
+
+
--- controllers/learning_motion_bayesian/learning_motion_bayesian.py
+++ controllers/learning_motion_bayesian/learning_motion_bayesian.py
@@ -32,10 +32,14 @@ import traceback
 import transforms3d
 import random
 import numpy as np
+import const
 import csv  # csvファイルの操作を楽にしてくれる
 from scipy.spatial import ConvexHull
 from types import SimpleNamespace
 from skopt import gp_minimize
+
+const.frame_parameter = 12
+const.moter_parameter = 18 #首のヨー軸は関係なさそうなので、抜きました
 
 # start the webots supervisor
 supervisor = Supervisor()
@@ -57,7 +61,7 @@ ball_translation = supervisor.getFromDef('BALL').getField('translation')
 ball_rotation = supervisor.getFromDef('BALL').getField('rotation')
 
 def file_read():
-    with open("../play_motion/kick_motion.csv", "r") as f:  # kick_motion.csvを読み込む
+    with open("../play_motion/before_optimize_kick.csv", "r") as f:  # kick_motion.csvを読み込む
         # delimiter = 区切り文字の指定, lineterminator = 改行文字の指定
         read_data = csv.reader(f, delimiter=",", lineterminator="\r\n")
         # リスト内包表記を使用し、read_dataからデータを二次元配列として取得
@@ -65,8 +69,8 @@ def file_read():
         return data
 
 def select_parameter(param, motiondata):
-    motiondata[6][0] = str(int(param[0])) #モーションファイルの6行0列目 = 6フレーム目の時間を指定している
-    motiondata[7][0] = str(int(param[1])) #モーションファイルの7行0列目 = 7フレーム目の時間を指定している
+    motiondata[6][0] = str(int(param[0])) #モーションファイルの6行0列目 = 5フレーム目の時間を指定している
+    motiondata[7][0] = str(int(param[1])) #モーションファイルの7行0列目 = 6フレーム目の時間を指定している
     motiondata[7][3] = str(int(param[2])) #モーションファイルの7行3列目 = 足先ピッチの角度を指定している
     motiondata[7][4] = str(int(param[3])) #モーションファイルの7行4列目 = 膝ピッチの角度を指定している
     return param
@@ -80,30 +84,17 @@ def file_update(motiondata):
 
 def func(param):  # 最適化関数の設定
     global player, ball, children, ball_translation, ball_rotation, supervisor  # グローバル変数の定義
-
-# ファイルの書き換えの処理
     motiondata = file_read()
     param = select_parameter(param, motiondata)
     file_update(motiondata)
 
-
-# ----------------
-#
     count = 0
     player.remove()
-    print("robot clean")
-    # GanekenKunの呼び出し 一度消して再生成することで新たなmotionファイルを対応させたロボットを呼び出す
-    # ここでGankenKunの位置座標の指定も行っている。
     children.importMFNodeFromString(-1, f'DEF PLAYER RoboCup_GankenKun {{translation -0.2 0.1 0.450 rotation 0 0 1 0 controller "play_motion" controllerArgs "motion.csv"}}')
-    # playerにsupervisorを与えている.(これをしないとsupervisorの.removeが使用できなくなる)
     player = supervisor.getFromDef('PLAYER')
-    # ボール物理情報を初期化
     ball.resetPhysics()
-    # ボールの位置を所定の位置に設置する(フィールドの真ん中に置く)
-    # [x, y, z] 単位は[m]
     ball_translation.setSFVec3f([0, 0, 0.1])
     ball_rotation.setSFRotation([0, 0, 1, 0])
-
     while supervisor.step(time_step) != -1:
         count += 1
         if count > 800:
@@ -115,8 +106,6 @@ def func(param):  # 最適化関数の設定
                 writer.writerow([param[0], param[1], param[2],param[3], pos[0], pos[1]])
     return -pos[0]
 
-
-
     # 各パラメータの探索範囲
 x0 = (1, 50)
 x1 = (1, 50)
@@ -124,6 +113,6 @@ x2 = (0, 80)
 x3 = (0, 80)
 # 探索空間の設定 この場合は4つの空間を探索している
 x = (x0, x1, x2, x3)
-result = gp_minimize(func, x, n_calls=100, noise=0.0,model_queue_size=1, verbose=True)
+result = gp_minimize(func, x, n_calls=1000, noise=0.0,model_queue_size=1, verbose=True)
 # n_call = サンプリング回数(試行回数) ここの数値分funcの処理を繰り返す。
 # verbose = 探索時の標準出力を切り替える
